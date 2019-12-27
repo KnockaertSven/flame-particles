241a6b5e8bf292b4cdd1fdfdbeaa59756f12850c
Sven Knockaert <sven.knockaert@hotmail.com>
2019-12-27 20:09:18 +0100

    Add ParticleManager to the main Game

--- lib/Game.dart
+++ lib/Game.dart
@@ -1,8 +1,10 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:particle_example/particles/ParticleManager.dart';
 
 class Game extends BaseGame {
   Paint _backgroundColor = Paint()..color = Colors.white;
+  ParticleManager _particleManager = ParticleManager();
   Size size;
 
   Game({@required this.size});
@@ -10,12 +12,17 @@ class Game extends BaseGame {
   @override
   render(Canvas canvas){
     super.render(canvas);
+
+    // (re)draw the background
     Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
     canvas.drawRect(rect, _backgroundColor);
+
+    _particleManager.render(canvas);
   }
 
   @override
   update(double t){
     super.update(t);
+    _particleManager.update();
   }
 }
